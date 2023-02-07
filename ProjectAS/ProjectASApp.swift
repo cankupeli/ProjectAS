324b75d3e7069902607023070f50d298b7aa9303
Eric Johansson <johanssonericwilliam@gmail.com>
2023-02-07 16:18:08 +0100

    Initial Commit

---
+++ ProjectAS/ProjectASApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectASApp.swift
+//  ProjectAS
+//
+//  Created by Eric Johansson on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ProjectASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectASUITests/ProjectASUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProjectASUITestsLaunchTests.swift
+//  ProjectASUITests
+//
+//  Created by Eric Johansson on 2023-02-07.
+//
+
+import XCTest
+
+final class ProjectASUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
